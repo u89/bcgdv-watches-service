36e0da1c674842ab017dc0b7ddf7aba5d8115288
Ahmed Salim <ahmed_alabadi@yahoo.com>
2022-09-04 16:44:13 +0400

    implemented the solution and passed the tests

--- controllers/store.js
+++ controllers/store.js
@@ -2,6 +2,7 @@
  * Here we handle the requests that come through the endpoints
  * and provide the expected response
  */
+const watchesModel = require('../models/watch')
 
 const checkout = (req, res) => {
     //handle the request and return the total price in the response
@@ -9,16 +10,47 @@ const checkout = (req, res) => {
     if(!watchIds || !watchIds.length) {
         res.status(400).send({message: 'Bad request'})
     } else {
-        res.send({price: 0})
+        const {total, watches} = calculateTotalPrice(watchIds);
+        res.send({price: total})
     }
 }
 
 const calculateTotalPrice = (watchIds) => {
     //accepts a list of watches (ids) and return the total price
+    //1. know the qty of each watch
+    const watchesWithQty = calculateQuantities(watchIds);
+    
+    const keys = Object.keys(watchesWithQty);
+    let total = 0;
+    let watches = [];
+    for (var i=0; i<keys.length; i++) {
+        //2. calculate the price for each one with discount
+        const watchId = keys[i];
+        const qty = watchesWithQty[watchId];
+        //get the watch with the applied discount from the watchModel
+        const watch = watchesModel.getWatchWithDiscount({watchId, qty});
+        if(watch) {
+            total += watch.amount - watch.discountAmount
+            watches.push(watch);
+        }
+    }
+    //3. calculate and return the total amount
+    return {total, watches};
 }
 
-const priceAfterDiscounts = (watchIds) => {
+const calculateQuantities = (productIds) => {
     //accepts a list of watches (ids) and return the total price after applying discount on each item
+    let products = {}; // a key:value object where key is the product id and value is the qty
+    for (const id of productIds) {
+        if(!products[id]) {
+            //add the product to the list with qty = 1
+            products[id] = 1
+        } else {
+            //increase the product qty by 1
+            products[id] += 1
+        }
+    }
+    return products;
 }
 
 module.exports = {
--- models/watch.js
+++ models/watch.js
@@ -13,6 +13,29 @@ const getWatches = (watchIds) => {
     return db.watches.find({ids: watchIds});
 }
 
+const getWatchWithDiscount = ({watchId, qty}) => {
+    const result = db.watches.find({ids: [watchId]});
+    if(result && result.length) {
+        const watch = result[0];
+        const amount = watch.unitPrice * qty; //amount before discount
+        if(!watch.discount || !watch.discount.type || !watch.discount.value) {
+            return {...watch, amount, discountAmount: 0}
+        }
+        switch(watch.discount.type.toLowerCase()) {
+            case 'x-for-y':
+                const [x, y] = watch.discount.value.split("-") //where y is the total price for each x watches
+                const discountCount = Math.floor(qty/x) //calculate how many times the discount can be applied on this qty
+                const finalAmount = discountCount * y + (qty - discountCount * x) * watch.unitPrice;
+                return {...watch, amount, discountAmount: amount - finalAmount} // will return the amount of the discount in case we need to display it to the customer
+            default:
+                return {...watch, amount, discountAmount: 0}
+        }
+    } else {
+        return null;
+    }
+}
+
 module.exports = {
     getWatches,
+    getWatchWithDiscount
 }
